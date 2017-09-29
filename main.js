2c6a1a08a499809529dd4311546e6d031d6615c4
matt66589 <32281119+matt66589@users.noreply.github.com>
2017-09-29 17:54:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+(function() {
+	$(document).ready(main);
+	
+	// Config for Firebase
+	var config = {
+    apiKey: "AIzaSyCnNTZOV9q_hh1WfgXAi95Mufl6kcNx64U",
+    authDomain: "simplechat-85df8.firebaseapp.com",
+    databaseURL: "https://simplechat-85df8.firebaseio.com",
+    projectId: "simplechat-85df8",
+    storageBucket: "",
+    messagingSenderId: "596433248868"
+  		};
+
+  	// Tells if We're Signed in or not
+	var signedin = false;
+	var currentUser;
+
+	// Main Function
+	function main() {
+		firebase.initializeApp(config);
+		$('.login-button').click(login);
+		$('.signup-button-on-page').click(signup);
+		}
+
+
+	// If Login Button is Clicked
+	function login() {
+		const auth = firebase.auth();
+		var email = $('.email-box').val();
+		var pass = $('.password-box').val();
+		
+			try {
+			auth.signInWithEmailAndPassword(email, pass);
+				}
+			
+			catch(e)
+			{
+				alert('Incorrect Password or user with email ' + email + ' has not been created');
+
+			}
+			// Checking to see if we're signed in
+			auth.onAuthStateChanged(firebaseUser => 
+				{
+					if(firebaseUser)
+					{
+						alert('Login Successful, Redirecting');
+						console.log('login successful, welcome!');
+						console.log(firebaseUser);
+						currentUser = firebaseUser;
+						window.location.replace("signedin.html");
+					}
+					else
+					{
+						
+					}
+				});
+
+	}
+
+	// If Signup Button is Clicked
+	function signup() {
+		const auth = firebase.auth();
+		var email = $('.email-box').val();
+		var pass = $('.password-box').val();
+		try {
+			auth.createUserWithEmailAndPassword(email, pass);
+		}
+		catch(e)
+		{
+
+		}
+		alert('account created, now login');
+		
+	}
+
+
+	/* USED TO USE THIS FUNCTION NOW IM THINKING ITS NOT SUCH A GOOD IDEA
+	function signinchanged()
+	{
+		if(signedin == false)
+		{
+			signedin = true;
+			alert('signin state changed, si');
+		}
+		else {
+			signedin = false;
+			alert('signin state changed');
+		}
+
+	}
+	*/
+
+})();
---
+++ signed.js
@@ -0,0 +1,54 @@
+(function() {
+	$(document).ready(main);
+	
+	// Config for Firebase
+	var config = {
+    apiKey: "AIzaSyCnNTZOV9q_hh1WfgXAi95Mufl6kcNx64U",
+    authDomain: "simplechat-85df8.firebaseapp.com",
+    databaseURL: "https://simplechat-85df8.firebaseio.com",
+    projectId: "simplechat-85df8",
+    storageBucket: "",
+    messagingSenderId: "596433248868"
+  		};
+
+  	// Tells if We're Signed in or not
+	var signedin = false;
+	var currentUser;
+
+	// Main Function
+	function main() {
+		firebase.initializeApp(config);
+		const auth = firebase.auth();
+		// Making sure the current user is actively signed in
+		auth.onAuthStateChanged(firebaseUser => 
+			{
+				
+
+				// Getting the info of the signed in user
+				var user = firebase.auth().currentUser;
+				var name, email, photoUrl, uid, emailVerified;
+
+				if(user != null)
+				{
+					name = user.displayName;
+					email = user.email;
+					photoUrl = user.photoUrl;
+					uid = user.uid;
+
+				}
+				else { alert('authentication lost please re-sign in.'); window.location.replace('index.html');}
+
+				currentUser = firebaseUser;
+				$('.welcome-user').text('Welcome ' + email);
+
+				// LEFT OFF RIGHT HERE
+			});
+			$('.signout-button').click(signout);
+		}
+
+	function signout()
+	{
+		firebase.auth().signOut();
+	}
+
+})();
