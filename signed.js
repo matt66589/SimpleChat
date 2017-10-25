e1b3fdb736eeeced6d67bf68329b66ac4c098fb6
Matthew Peterson <32281119+matt66589@users.noreply.github.com>
2017-10-24 20:46:22 -0400

    Add files via upload
    Working on database querying functionality

--- signed.js
+++ signed.js
@@ -14,40 +14,60 @@
   	// Tells if We're Signed in or not
 	var signedin = false;
 	var currentUser;
+	var alluserssearch = false;
 
 	// Main Function
 	function main() {
-		firebase.initializeApp(config);
-		const auth = firebase.auth();
+		if(alluserssearch)
+		{
+			// WRiTE THE STUFF FOR SEARHCHING THE DATABASE
+			// Make sure to have it false when its doe with searching the fatabase
+			var ref = firebase.database().ref('users');
+			var events = ref.child('email_id');
+			
+			
 
-		// Making sure the current user is actively signed in
-		auth.onAuthStateChanged(firebaseUser => 
-			{
-				
-
-				// Getting the info of the signed in user
-				var user = firebase.auth().currentUser;
-				var name, email, photoUrl, uid, emailVerified;
-
-				if(user != null)
-				{
-					name = user.displayName;
-					email = user.email;
-					photoUrl = user.photoUrl;
-					uid = user.uid;
-
-				}
-				else { alert('authentication lost please re-sign in.'); window.location.replace('index.html');}
-
-				currentUser = firebaseUser;
-				$('.welcome-user').text('Welcome ' + email);
-
-				// LEFT OFF RIGHT HERE
-			});
-			$('.signout-button').click(signout);
-			$('.send-message').click(sendMessage);
-			$('.back-button').click(backButton);
+			
 		}
+		else
+		{
+
+
+			firebase.initializeApp(config);
+			const auth = firebase.auth();
+
+
+			// Making sure the current user is actively signed in
+			auth.onAuthStateChanged(firebaseUser => 
+				{
+					
+
+					// Getting the info of the signed in user
+					var user = firebase.auth().currentUser;
+					var name, email, photoUrl, uid, emailVerified;
+
+					if(user != null)
+					{
+						name = user.displayName;
+						email = user.email;
+						photoUrl = user.photoUrl;
+						uid = user.uid;
+
+					}
+					else { alert('authentication lost please re-sign in.'); window.location.replace('index.html');}
+
+					currentUser = firebaseUser;
+					$('.welcome-user').text('Welcome ' + email);
+
+					// LEFT OFF RIGHT HERE
+				});
+				$('.signout-button').click(signout);
+				$('.send-message').click(sendMessage);
+				$('.back-button').click(backButton);
+				$('.all-users').click(allUsersButton);
+			}
+		}
+
 
 	function signout()
 	{
@@ -62,5 +82,10 @@
 	function backButton()
 	{
 		window.location.replace("signedin.html")
+	}
+	function allUsersButton()
+	{
+		window.location.replace("allusers.html");
+		alluserssearch = true;
 	}
 })();
