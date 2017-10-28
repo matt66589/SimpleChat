86c944ded23347c0f1289f9e28358d3ad461f1eb
Matthew Peterson <32281119+matt66589@users.noreply.github.com>
2017-10-27 23:53:14 -0400

    Add files via upload
    fixed error in Allusers page but still need to add more functionality.

--- search.js
+++ search.js
@@ -11,23 +11,44 @@
     messagingSenderId: "596433248868"
   		};
 
-  	
+  	var userDataTree = new Array(10);
 
 	function main()
 	{
 		// First were gonna check if we're still authenticated
+		firebase.initializeApp(config);
 		$('.back-button').click(back);
-		firebase.InitializeApp(config);
 		const auth = firebase.auth();
 		// make sure we are still signed in 
-		auth.OnAuthStateChanged(firebaseuser => 
+		auth.onAuthStateChanged(firebaseuser => 
 			{
 				var user = firebase.auth().currentUser;
 
 				if(user != null)
 				{
 					// search database stuff right here
-					$('#result-text').text("Loading");
+					$('#result-text').text("Loading...");
+					var ref = firebase.database().ref("users");
+					ref.on("child_added", function(snapshot) 
+					{
+						
+						snapshot.forEach(function (userData) 
+							{
+								
+								// attempting to display the user data hopelessley
+								// maybe im not filtering the right data???
+								// need to search for email_id but cant get directly there without getting
+								// around the user toek thing. 
+								var dataCounter = 0;
+								var converted = userData.val();
+								userDataTree[dataCounter] = converted;
+								dataCounter++;
+								
+							});
+							
+					});
+
+
 				}
 				else
 				{
